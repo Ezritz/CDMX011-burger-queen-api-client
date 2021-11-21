83c99a796b30d44ea5ae81beca68a161ff0891f1
ezritz <lidia.edith.escoto@gmail.com>
2021-11-20 21:06:37 -0600

    Add auth state

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -7,53 +7,55 @@ import { useAuthContext } from './hooks/useAuthContext';
 
 function OrdersRoute({ children }) {
   const { user } = useAuthContext();
-  return user ? children : <Navigate to='/login' />
+  return user ? children : <Navigate to='/login'/>
 }
 
 function LoginRoute({ children }) {
   const { user } = useAuthContext();
-  return user ? <Navigate to='/orders' />: children;
+  return user ? <Navigate to='/orders'/>: children;
 }
 
 function HomeRoute() {
   const { user } = useAuthContext();
-  return user ? <Navigate to='/orders'/>: <Navigate to='/login' />
+  return user ? <Navigate to='/orders'/>: <Navigate to='/login'/>
 }
 
 function AppRoutes() {
-  
-
+  const { authIsReady } = useAuthContext();
   return (
+
+
     <div className="App">
-      
-      <BrowserRouter>
-        <Routes>
+      {authIsReady && (
+        <BrowserRouter>
+          <Routes>
+            <Route 
+              path="/orders" 
+              element={
+                <OrdersRoute>
+                  <Orders />
+                </OrdersRoute>
+              }/>
+
+            <Route 
+              path='/login' 
+              element={
+                <LoginRoute>
+                  <Login />
+                </LoginRoute>
+              }/>
+
+            <Route 
+              path="/"
+              element={
+                <HomeRoute/>
+              }/>
+              
+          </Routes>
           
-          <Route 
-            path="/"
-            element={
-              <HomeRoute/>
-            }/>
-
-          <Route 
-            path="/orders" 
-            element={
-              <OrdersRoute>
-                <Orders />
-              </OrdersRoute>
-            }/>
-
-          <Route 
-            path='/login' 
-            element={
-              <LoginRoute>
-                <Login />
-              </LoginRoute>
-            }/>
-            
-        </Routes>
-        
-      </BrowserRouter>
+        </BrowserRouter>
+      )}
+      
     </div>
   );
 }
