cc60c89890a38e65b8e469fa9a2d2e58f42c4ecf
ezritz <lidia.edith.escoto@gmail.com>
2021-11-22 18:51:06 -0600

    Add imgs and button breakfast working

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,11 +1,34 @@
 import { useLogout } from '../hooks/useLogout';
+import myImage from '../images/logoMesero.png';
+import notification from '../images/notification.png';
+import Breakfast from './Breakfast';
+import { useState } from 'react';
+
 
 export default function Orders() {
   const { logout } = useLogout();
+  const [showBreakf, setShowBreakf] = useState(false);
+  const onClick = () => setShowBreakf(true);
   return(
-    <div>
-      <h2>Home</h2>
-      <button onClick={logout}>Salir</button>
+    
+    <div className="container">
+      
+      <div className="header">
+        <img src={myImage} alt="img-waiter" />
+        <div>
+          <p></p>
+          <button onClick={logout}>Cerrar Sesión</button>
+          
+        </div>
+        <div>
+          <img src={notification} alt="img-notification" />
+        </div>
+        <button onClick={onClick}>Desayuno</button>
+        {showBreakf ? <Breakfast /> :null}
+        <button>Almuerzo y Cena</button>
+      </div>
+      
     </div>
+    
   )
 }
