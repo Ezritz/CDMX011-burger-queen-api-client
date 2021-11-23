8974655b2764661764252d0bab216aadeb1572fb
ezritz <lidia.edith.escoto@gmail.com>
2021-11-22 22:40:17 -0600

    Change in menu buttons

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,31 +1,53 @@
 import { useLogout } from '../hooks/useLogout';
+import { useState } from 'react';
 import myImage from '../images/logoMesero.png';
 import notification from '../images/notification.png';
 import Breakfast from './Breakfast';
-import { useState } from 'react';
+import Lunch from './Lunch';
+import styles from '../css/orders/Orders.scss';
+
+// import { useState } from 'react';
 
 
 export default function Orders() {
   const { logout } = useLogout();
-  const [showBreakf, setShowBreakf] = useState(false);
-  const onClick = () => setShowBreakf(true);
+  const [activeMenu, setActiveMenu] = useState('');
+  // const [showBreakf, setShowBreakf] = useState(false);
+  // const onClick = () => setShowBreakf(true);
   return(
     
     <div className="container">
       
       <div className="header">
-        <img src={myImage} alt="img-waiter" />
+        <img src={myImage} alt="img-waiter" className="user"/>
         <div>
           <p></p>
           <button onClick={logout}>Cerrar Sesión</button>
           
         </div>
-        <div>
-          <img src={notification} alt="img-notification" />
+        <div className="cont-ntf">
+          <img src={notification} alt="img-notification" className="ntf"/>
         </div>
-        <button onClick={onClick}>Desayuno</button>
-        {showBreakf ? <Breakfast /> :null}
-        <button>Almuerzo y Cena</button>
+        
+        <button 
+        type="button"
+        onClick={() => setActiveMenu('breakfast')}>
+        Desayuno</button>
+        
+        <button
+        type="button"
+        onClick={() => setActiveMenu('lunch')}>
+        Almuerzo y Cena</button>
+       
+        <div>
+          {activeMenu === 'breakfast' && (
+            <Breakfast/>
+          )}
+          {activeMenu === 'lunch' && (
+            <Lunch/>
+          )}
+        </div>
+        
       </div>
       
     </div>
