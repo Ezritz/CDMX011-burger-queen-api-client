00e873e15e7daa22a390f9af919ff5f5f3ae2308
Isabel <isvaoc@gmail.com>
2021-11-23 18:44:13 -0600

    añadiendo estilo a la caja de los alimentos

--- src/components/DrinksBreakfast.js
+++ src/components/DrinksBreakfast.js
@@ -1,11 +1,11 @@
-//
+import '../css/orders/DrinksBreakfast.scss';
 import menu from '../data';
 import BreakfastMenu from './BreakfastMenu';
 
 export default function drinksBreakfast() {
     let drinksBreak = menu.filter((elem) => elem.type === 'Bebida-desayuno');
     let menuDrinks = drinksBreak.map((element) => {
-        return <BreakfastMenu image={element.img} text={element.description + " " + element.Price} id={"image-drinks"}/>
+        return <BreakfastMenu image={element.img} text={element.description + " " + element.Price} className={"image-drinks"}/>
     });
 
     return (
