e214021a53ec497f898062f6a651ba65c50c58fc
ezritz <lidia.edith.escoto@gmail.com>
2021-11-26 11:22:47 -0600

    Change data

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -1,13 +1,7 @@
 import '../css/orders/Breakfast.scss';
-import menu from '../data';
 import DrinksBreakfast from './DrinksBreakfast';
 import Sandwich from './Sandwich';
-import Comanda from './Comanda';
 import '../css/orders/Comanda.scss';
-
-let menuArray = menu.filter((elem)=> elem.type === 'Sandwich');
-  
-console.log('hlis',menuArray);
 
 export default function Breakfast () {
   
