a1c504f4e005d5e85d5d9a026a289403712e2cf9
ezritz <lidia.edith.escoto@gmail.com>
2021-11-25 18:46:40 -0600

    Save client name and show in comanda

--- src/components/Comanda.js
+++ src/components/Comanda.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
+
 export default function Comanda() {
+  const [clientName, setClientName] = useState('');
+
+  const resetComanda = () => {
+    setClientName('');
+  }
+
 
   return (
     <div className="comanda">
       <p className="title-comanda">Comanda</p>
-      <input type="text" placeholder="Nombre Cliente" id="client-name"></input>
+      <input 
+      type="text" 
+      placeholder="Nombre Cliente"
+      onChange={(e) => setClientName(e.target.value)}
+      value={clientName}
+      id="client-name"></input>
       <div id="total-products">
         <table className="table">
           <thead>
@@ -21,11 +34,16 @@ export default function Comanda() {
         <tr className="total">
           <p className="total">Total</p>
           <p className="total">$0</p>
+          <p className="total">{clientName}</p>
         </tr>
       </div>
       <div className="btns-comanda">
-        <button className="btn-comanda">Enviar comanda</button>
-        <button className="btn-cancelar">Cancelar</button>
+        <button
+        className="btn-comanda">Enviar comanda</button>
+        <button 
+        className="btn-cancelar"
+        onClick={resetComanda}
+        >Cancelar</button>
       </div>
       
     </div>  
